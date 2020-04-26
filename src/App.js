533d20dd66775787809f3709d69ae7a6fe395889
Parth Sarthy <parthsarthy991@gmail.com>
2020-04-27 00:44:36 +0100

    add hooks, useeffect, fetch data asynchronously

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
-import Pet from './Pet'
+// import Pet from './Pet'
+import SearchParams from './SearchParams'
 
 const App = () => {
     return (
         <div>
             <h1 id="something-important">Adopt Me!</h1>
-            <Pet name="Luna" animal="Dog" breed="Havanese" />
+            <SearchParams />
+            {/* <Pet name="Luna" animal="Dog" breed="Havanese" />
             <Pet name="Pepper" animal="Bird" breed="Cocktail" />
-            <Pet name="Doink" animal="Cat" breed="Mixed" />
+            <Pet name="Doink" animal="Cat" breed="Mixed" /> */}
         </div>
     )
 }
